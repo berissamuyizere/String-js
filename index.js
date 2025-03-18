cf864bbae80ef66dc15dafca878f0b35257aba1e
BerissaMuyizere <muyizereberissa@gmail.com>
2025-03-18 06:00:05 +0300

    finding words

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ console.log({lastFour});
 let text = "The quick fox jumped over the lazy dog";
 let newText = text.slice(0, 4) + " eat " + text.slice(4);
 console.log(newText);
+
+// Find specific words in strings
+let string1 = "The pupils are reading in the library";
+let string2 = "The child was sitting on the table before it fell";
+let findAre = string1.includes("are");
+let findSitting = string2.includes("sitting");
+console.log(`"are" found: ${findAre}`); 
+console.log(`"sitting" found: ${findSitting}`);
