ec44c97b75a20edbe48fbf97b750e67b0c37e50e
BerissaMuyizere <muyizereberissa@gmail.com>
2025-03-18 05:57:52 +0300

    inserting index

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 let word = "extravaganza";
 let lastFour = word.slice(-4);
 console.log({lastFour}); 
+
+// Insert "eat" at the fourth index in the given string
+let text = "The quick fox jumped over the lazy dog";
+let newText = text.slice(0, 4) + " eat " + text.slice(4);
+console.log(newText);
